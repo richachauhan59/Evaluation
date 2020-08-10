4e1490c844f5a7a914e6cb6e5bfcab9cb69964e4
Richa Chuahan <richachauhan2019@gmail.com>
2020-08-10 15:57:38 +0530

    User Details

---
+++ src/coding_1/name.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import data from '../data.json'
+import UserDetails from './userDetails'
+
+export default class Name extends React.Component {
+    constructor(props){
+        super(props)
+
+ }
+    render(){
+        return(
+            <div  >
+
+                {
+                    data.map(item =>
+
+                        <UserDetails
+
+                            key={item.address}
+                            img={item.avatar}
+                            f_name={item.first_name}
+                            l_name={item.last_name}
+                            address={item.address}
+                            karma={item.karma}
+                            followers={item.followers}
+                            posts={item.posts}
+                            isfollowing={item.is_following}
+
+                        />
+                    )
+                }
+
+            </div>
+        )
+    }
+}
---
+++ src/coding_1/userDetails.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+// import styles from './UserDetails.module.css'
+
+function UserDetails(props) {
+    return (
+
+        <div style={{display:"flex", width:"90%", textAlign: 'center', padding:'2%', border: " 1px solid black", margin:"2%"}} >
+
+            <div>
+                <img style={{borderRadius: "50%"}}  src={props.img} alt="img"></img>
+            </div>
+
+            <div>
+                <h2>{props.f_name + " " + props.l_name}</h2>
+                <div >
+                    {props.address}
+                </div>
+            </div>
+
+            <div>
+                <h1>{props.karma}</h1>
+                <div>Karma</div>
+            </div>
+
+            <div>
+                <h1>{props.followers}</h1>
+                <div>Followers</div>
+            </div>
+
+            <div>
+                <h1>{props.posts}</h1>
+                <div>Posts</div>
+            </div>
+
+            <div>
+                <button>{props.isfollowing ? "Unfollow" : "Follow"}</button>
+            </div>
+
+        </div>
+    )
+}
+
+export default UserDetails
